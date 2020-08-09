6f3fb23299b104d6e90f480c991ebe11f73e0604
Andrew Bautista <andrewbautista85@gmail.com>
2020-08-09 17:17:58 -0400

    removed Modal from App, moving it to portfolio

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class App extends Component {
         <About resumeData={resumeData} />
         <Resume resumeData={resumeData} />
         <Portfolio resumeData={resumeData} />
-        <Modal resumeData={resumeData} />
         <Testimonials resumeData={resumeData} />
         <ContactUs resumeData={resumeData} />
         <Footer resumeData={resumeData} />
