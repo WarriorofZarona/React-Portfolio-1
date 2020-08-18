ed5cca6f3459908e11c5dc45de68ff95e6099d99
Andrew Bautista <andrewbautista85@gmail.com>
2020-08-18 11:58:06 -0400

    added css to center the content of the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,10 +7,12 @@ export default class Modal extends Component {
             // <div className="mfp-fade mfp-wrap">
             //     <div className="mfp-content">
             <div className="popup-modal">
-                <div className="description-box">
-                    <h4>Testing</h4>
-                    <p>This is a test</p>
-                    <a href="#">This is a clickable link</a>
+                <div className="modal-content">
+                    <div className="description-box">
+                        <h4>Testing</h4>
+                        <p>This is a test</p>
+                        <a href="#">This is a clickable link</a>
+                    </div>
                 </div>
             </div>
             //     <g
