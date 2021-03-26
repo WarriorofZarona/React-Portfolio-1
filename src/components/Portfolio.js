86da9729e5a3b655093e6c879b15765822d3b0ef
Andrew Bautista <andrewbautista85@gmail.com>
2021-03-25 21:43:40 -0400

    refactored from class components to functional components

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,60 +1,57 @@
-import React, { Component } from 'react';
-import Modal from './Modal';
-export default class Porfolio extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalIsOpen: false,
-      item: {}
-    };
-    this.handleClose = e => {
-      e.stopPropagation();
-      e.preventDefault();
-      console.log("Is this working?")
-      this.setState({ modalIsOpen: false })
-    };
-    this.handleClick = (item) => {
-      // e.preventDefault();
-      // e.stopPropagation();
-      console.log("Hello world!")
-      this.setState({ item: item })
-      this.setState({ modalIsOpen: true });
-    }
-  }
+import React, { useState } from "react";
+import Modal from "./Modal";
 
-  render() {
-    let resumeData = this.props.resumeData;
-    //need a function that handles the onClick event so that it passes the targeted project data to the modal
+const Portfolio = ({ resumeData }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [portfolioObject, setPortfolioObject] = useState({});
 
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
 
-    return (
-      <section id="portfolio">
-        <div className="row">
-          <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Works.</h1>
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {
-                resumeData.portfolio && resumeData.portfolio.map((item, index, array) => {
-                  return (
-                    <div className="columns portfolio-item" key={index}>
-                      <div className="item-wrap">
-                        <img src={`${item.imgurl}`} className="item-img" />
-                        <div className="overlay" onClick={() => this.handleClick(item)} >
-                          <div className="portfolio-item-meta">
-                            <h5>{item.name}</h5>
-                            <p>{item.description}</p>
-                          </div>
-                        </div>
+  const handleClick = (obj) => {
+    setPortfolioObject(obj);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-quarters s-bgrid-thirds cf"
+          >
+            {resumeData.portfolio.map((item, index, array) => {
+              return (
+                <div className="columns portfolio-item" key={index}>
+                  <div className="item-wrap">
+                    <img
+                      src={`${item.imgurl}`}
+                      alt={item.name}
+                      className="item-img"
+                    />
+                    <div className="overlay" onClick={() => handleClick(item)}>
+                      <div className="portfolio-item-meta">
+                        <h5>{item.name}</h5>
+                        <p>{item.description}</p>
                       </div>
                     </div>
-                  )
-                })
-              }
-              {this.state.modalIsOpen ? <Modal item={this.state.item} handleClose={this.handleClose} /> : <div />}
-            </div>
+                  </div>
+                </div>
+              );
+            })}
+            {isModalOpen ? (
+              <Modal item={portfolioObject} handleClose={handleClose} />
+            ) : (
+              <div />
+            )}
           </div>
         </div>
-      </section >
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,105 +1,104 @@
-import React, { Component } from 'react';
-export default class Resume extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <section id="resume">
+import React from "react";
+const Resume = ({ resumeData }) => {
+  return (
+    <section id="resume">
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
 
-        <div className="row education">
+        <div className="nine columns main-col">
+          {resumeData.education &&
+            resumeData.education.map((item) => {
+              return (
+                <div className="row item">
+                  <div className="twelve columns">
+                    <h3>{item.UniversityName}</h3>
+                    <p className="info">
+                      {item.specialization}
+                      <span>&bull;</span>{" "}
+                      <em className="date">
+                        {item.MonthOfPassing} {item.YearOfPassing}
+                      </em>
+                    </p>
+                    <p className="description">{item.Description}</p>
 
-          <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-          </div>
-
-          <div className="nine columns main-col">
-            {
-              resumeData.education && resumeData.education.map((item) => {
-                return (
-                  <div className="row item">
-                    <div className="twelve columns">
-                      <h3>{item.UniversityName}</h3>
-                      <p className="info">
-                        {item.specialization}
-                        <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                      <p className="description">{item.Description}</p>
-
-                      {item.Achievements.map(achievement => (
-                        <ul>
-                          <li>{achievement}</li>
-                        </ul>
-                      ))}
-
-                    </div>
+                    {item.Achievements.map((achievement) => (
+                      <ul>
+                        <li>{achievement}</li>
+                      </ul>
+                    ))}
                   </div>
-                )
-              })
-            }
-          </div>
+                </div>
+              );
+            })}
         </div>
-        <div className="row work">
-          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-          </div>
+      </div>
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Work</span>
+          </h1>
+        </div>
 
-          <div className="nine columns main-col">
-            {
-              resumeData.work && resumeData.work.map((item) => {
-                return (
-                  <div className="row item">
-                    <div className="twelve columns">
-                      <h3>{item.CompanyName}</h3>
-                      <p className="info">
-                        {item.specialization}
-                        <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
+        <div className="nine columns main-col">
+          {resumeData.work &&
+            resumeData.work.map((item) => {
+              return (
+                <div className="row item">
+                  <div className="twelve columns">
+                    <h3>{item.CompanyName}</h3>
+                    <p className="info">
+                      {item.specialization}
+                      <span>&bull;</span>{" "}
+                      <em className="date">
+                        {item.MonthOfLeaving} {item.YearOfLeaving}
+                      </em>
+                    </p>
 
-                      {item.Achievements.map(achievement => (
-                        <ul>
-                          <li>{achievement}</li>
-                        </ul>
-                      ))}
-
-                    </div>
-
+                    {item.Achievements.map((achievement) => (
+                      <ul>
+                        <li>{achievement}</li>
+                      </ul>
+                    ))}
                   </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
 
-                )
-              })
-            }
-          </div>
+      <div className="row skill">
+        <div className="three columns header-col">
+          <h1>
+            <span>Skills</span>
+          </h1>
         </div>
 
+        <div className="nine columns main-col">
+          <p>{resumeData.skillsDescription}</p>
 
-        <div className="row skill">
-
-          <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
+          <div className="skills">
+            {resumeData.skills &&
+              resumeData.skills.map((item) => {
+                return (
+                  <span className="skill">
+                    <img
+                      class="skill-logo"
+                      src={item.image}
+                      alt={item.skillname}
+                      title={item.skillname}
+                    />
+                  </span>
+                );
+              })}
           </div>
-
-          <div className="nine columns main-col">
-
-            <p>
-              {resumeData.skillsDescription}
-            </p>
-
-            <div className="skills">
-
-              {
-                resumeData.skills && resumeData.skills.map((item) => {
-                  return (
-                    <span className="skill">
-                      <img class="skill-logo" src={item.image} alt={item.skillname} title={item.skillname} />
-                    </span>
-                  )
-                })
-              }
-
-            </div>
-
-          </div>
-
         </div>
+      </div>
+    </section>
+  );
+};
 
-      </section>
-    );
-  }
-}
+export default Resume;
